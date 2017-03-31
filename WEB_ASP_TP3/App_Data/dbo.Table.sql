87e8094d3f6ddb3a8693e8b606869591c3b093b1
gustavofalmeida <gustavo.falmeida@al.infnet.edu.br>
2017-03-31 12:08:40 -0300

    v2

---
+++ WEB_ASP_TP3/App_Data/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].Emprestimo
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [dataEmprestimo] NVARCHAR(150) NULL, 
+    [dataDevolucao] NVARCHAR(150) NULL, 
+    [idLivro] INT NULL, 
+    CONSTRAINT [FK_idLivro] FOREIGN KEY ([idlivro]) REFERENCES [livro]([id])
+	
+)
+
